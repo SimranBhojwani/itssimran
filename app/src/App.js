4b31d530e2dfbc37c68abfa49512d7fcff1ea02e
Simran Bhojwani <simran.bhojwani187@gmail.com>
2022-03-28 02:12:20 +0530

    replace BrowserRouter with HashRouter

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bulma/css/bulma.min.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from './pages/home';
