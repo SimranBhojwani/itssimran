f3d610389f97da5adb4f868211f89e5a07084b45
Simran Bhojwani <simran.bhojwani187@gmail.com>
2022-03-28 00:17:42 +0530

    commit for its simran

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from './pages/home';
+import Work from './pages/work';
+import Quotes from './pages/quotes';
+import About from './pages/about';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+         <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/work' element={<Work/>} />
+          <Route path='/quotes' element={<Quotes/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes> 
+        <Footer/> 
+       </Router>
+     
     </div>
   );
 }
