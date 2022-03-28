78aaa0bc4cf73d2f296801e49f28df5c8f9c384a
Simran Bhojwani <simran.bhojwani187@gmail.com>
2022-03-28 14:20:54 +0530

    minor changes in home page

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -31,7 +31,7 @@ const Home = () => {
                     <div class="container is-fluid" style={{transform: `translateY(${offset * 0.3}px)`}} >
                         <p class="mb-10 is-size-1 has-text-centered">Hey, Its Simran Here !</p>
                         <br></br>
-                        <p class="is-size-4 has-text-justified is-italic">"Welcome to my homepage. You can probably find me doing either of these activities mentioned in the iconic frame. Apart from that, I find joy in the little things of life and believes in simplicity."</p>
+                        <p class="is-size-4 has-text-justified is-italic">"Welcome to my home<span class="has-text-grey-light">page</span>. You can probably find me doing either of these activities mentioned in the iconic frame. Apart from that, I find joy in the little things of life and believes in simplicity."</p>
                         <br></br>
                         <p class="is-size-5 has-text-centered has-text-weight-light is-family-monospace"><a  href="#/about">MoreAboutMe :)</a></p>
                     </div>
