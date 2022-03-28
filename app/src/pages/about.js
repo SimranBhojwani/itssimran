48317e1db84548ee131024e0a8ca585e0bbfba9b
Simran Bhojwani <simran.bhojwani187@gmail.com>
2022-03-28 15:04:33 +0530

    text justify

--- app/src/pages/about.js
+++ app/src/pages/about.js
@@ -13,8 +13,8 @@ const About = () => {
             </Section> 
             <div class="container is-fluid">
                 <div class="container">
-                    <p class="is-size-5 ">
-                        I have done my graduation in Information Technology. I'm currently working as Sr. SDET. My interest lies in less is more ideology. I apply this idea in almost everything that I do. And my inclination towards analyzing the design of everyday things feeds my curiosity of why things are how they are. Is observe this beautiful creation of ours and try to understand different perspective because no problem can be solved from the same level of consiousness that created it. I spend my time balancing logic and creativity, health and work, todo and tomorrow.Blessed with loving parents and friends I like to spend my time with them too. My God, writing about yourself is like finding words in an empty dictionary.
+                    <p class="is-size-5 has-text-justified">
+                        I have done my graduation in Information Technology. I'm currently working as Sr. SDET. My interest lies in less is more ideology. I apply it in almost everything that I do. And my inclination towards analyzing the design of everyday things feeds my curiosity of why things are how they are. I observe this beautiful creation of ours and try to understand different perspective because no problem can be solved from the same level of consiousness that created it. I spend my time balancing logic and creativity, health and work, todo and tomorrow. Blessed with loving parents and friends I like to spend my time with them too. My God, writing about yourself is like finding words in an empty dictionary.
                     </p>
                     <br></br>
                     <p class="is-size-5">
