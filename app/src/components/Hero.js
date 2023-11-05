0da96414df803e30d37a66827e382f915a14d4a9
Simran Bhojwani <simran@Simrans-MacBook-Pro.local>
2023-11-05 13:48:24 +0530

    mobile responsive fixes

--- app/src/components/Hero.js
+++ app/src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className='content'>
         <img src={simran} alt='Simran Bhojwani Profile'/>
             <div className='col-1'>
-            <p> Hi I'm</p>
+              <p> Hi I'm</p>
               <p>Simran Bhojwani</p>
               <p>Nice to meet you :) </p>
               <Link to="work" spy={true} smooth={true} offset={-100} duration={500}><button className='button'>Scroll for more</button></Link>
