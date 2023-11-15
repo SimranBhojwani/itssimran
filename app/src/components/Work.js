8eea7be403dca1bfca897ad2e516a6f4b102edfd
Simran Bhojwani <simran@Simrans-MacBook-Pro.local>
2023-11-15 17:27:06 +0530

    add new work

--- app/src/components/Work.js
+++ app/src/components/Work.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Work.css'
 import irctc from './../assets/work/irctc.png'
 import navbar from './../assets/work/navbar.png'
-import testimonials from './../assets/work/testimonials.png'
+import testimonials from './../assets/work/ola.png'
 
 const Work = () => {
   return (
@@ -11,7 +11,15 @@ const Work = () => {
         <h2>Selected Work</h2>
         <span className='line'></span>
         <div className='content'>
-            <a href="https://dribbble.com/shots/22892445-IRCTC-Redesign-casestudy">
+          <a href="https://dribbble.com/shots/23054899-Ola-Redesign-Casestudy?utm_source=Clipboard_Shot&utm_campaign=SimranBhojwani&utm_content=Ola%20Redesign%20Casestudy&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=SimranBhojwani&utm_content=Ola%20Redesign%20Casestudy&utm_medium=Social_Share">
+            <div className='card'>
+                <img src={testimonials} alt='ola casestudy'/>
+                <p>Improved ride search accessibility</p>
+                <p><span>Ola Redesign</span></p>
+                <p>Casestudy</p>
+            </div>
+            </a>
+            <a href="https://dribbble.com/shots/22892445-IRCTC-Redesign-casestudy?utm_source=Clipboard_Shot&utm_campaign=SimranBhojwani&utm_content=IRCTC%20Redesign%20casestudy&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=SimranBhojwani&utm_content=IRCTC%20Redesign%20casestudy&utm_medium=Social_Share">
               <div className='card'>
                   <img src={irctc} alt='irctc casestudy'/>
                   <p>Simplified train search</p>
@@ -19,7 +27,7 @@ const Work = () => {
                   <p>Casestudy</p>
               </div>
             </a>
-            <a href="https://dribbble.com/shots/22822556-Website-Navigation">
+            <a href="https://dribbble.com/shots/22822556-Website-Navigation?utm_source=Clipboard_Shot&utm_campaign=SimranBhojwani&utm_content=Website%20Navigation&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=SimranBhojwani&utm_content=Website%20Navigation&utm_medium=Social_Share">
               <div className='card'>
                   <img src={navbar} alt='saas navbar'/>
                   <p>Dynamic website navigation</p>
@@ -27,14 +35,7 @@ const Work = () => {
                   <p>Daily UI #053</p>
               </div>
             </a>
-            <a href="https://dribbble.com/shots/22714707-Testimonials">
-            <div className='card'>
-                <img src={testimonials} alt='testimonials'/>
-                <p>Attractive customer testimonials</p>
-                <p><span>Visual Design</span></p>
-                <p>Daily UI #039</p>
-            </div>
-            </a>
+            
         </div>
         <a href="https://dribbble.com/SimranBhojwani">
           <p>View Dribbble &nbsp; &rarr;</p>
